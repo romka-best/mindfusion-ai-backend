f66c61b1005d1da8300d378cb09b33d55d0f5529
romka-best <romka.best2016@yandex.ru>
2025-04-19 02:09:16 -0700

    clean up

--- bot/keyboards/settings/settings.py
+++ bot/keyboards/settings/settings.py
@@ -20,7 +20,7 @@ from bot.database.models.common import (
     RunwayResolution,
     RunwayDuration,
     LumaRayQuality,
-    LumaRayDuration, KlingVersion,
+    LumaRayDuration,
 )
 from bot.database.models.user import UserSettings, UserGender
 from bot.locales.main import get_localization
