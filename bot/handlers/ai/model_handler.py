dfcf0997f473a7b7c9d381a777b4c683df746a37
PeterRunich <43861241+PeterRunich@users.noreply.github.com>
2025-05-15 10:23:27 +0300

    useless import

--- bot/handlers/ai/model_handler.py
+++ bot/handlers/ai/model_handler.py
@@ -5,7 +5,6 @@ from aiogram.exceptions import TelegramBadRequest, TelegramRetryAfter
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
-from proto import message
 
 from bot.config import config, MessageEffect
 from bot.database.models.common import Model, ModelType
