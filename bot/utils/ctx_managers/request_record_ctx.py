dcb44a3e1d088da306890b05360a71fb84a6c1eb
Peter Runich <p.runich@bk.ru>
2025-05-18 10:27:58 +0300

    Added ctxs managers, for processing imgs, make request and generation

---
+++ bot/utils/ctx_managers/request_record_ctx.py
@@ -0,0 +1,24 @@
+from contextlib import AsyncContextDecorator
+
+from bot.database.models.request import RequestStatus
+from bot.database.operations.request.updaters import update_request
+from bot.database.operations.request.writers import write_request
+
+
+class RequestRecordCtx(AsyncContextDecorator):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    async def __aenter__(self):
+        self.request = await write_request(*self.args, **self.kwargs)
+
+        return self
+
+    async def __aexit__(self, exc_type, *args):
+        if exc_type is not None:
+            self.request.status = RequestStatus.FINISHED
+
+            await update_request(self.request.id, {"status": self.request.status})
+            print(f"!!!!!DELETED {self.request}")
+
