f072ff28efda4a4dfcfde3640d643f5f7f863f72
Peter Runich <p.runich@bk.ru>
2025-05-19 12:28:39 +0300

    Stable diff use ctxs

--- bot/handlers/ai/stable_diffusion_handler.py
+++ bot/handlers/ai/stable_diffusion_handler.py
@@ -1,4 +1,5 @@
 import asyncio
+from contextlib import AsyncExitStack
 from datetime import datetime, timezone
 from typing import Optional
 
@@ -6,36 +7,33 @@ from aiogram import Router
 from aiogram.exceptions import TelegramBadRequest, TelegramRetryAfter
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
 from aiogram.utils.chat_action import ChatActionSender
+from replicate.exceptions import ReplicateError
 
-from bot.config import config, MessageEffect, MessageSticker
+from bot.config import MessageEffect, MessageSticker, config
 from bot.database.main import firebase
 from bot.database.models.common import Model, Quota, StableDiffusionVersion
-from bot.database.models.generation import GenerationStatus
-from bot.database.models.request import RequestStatus
 from bot.database.models.user import User, UserSettings
-from bot.database.operations.generation.getters import get_generations_by_request_id
-from bot.database.operations.generation.updaters import update_generation
 from bot.database.operations.generation.writers import write_generation
 from bot.database.operations.product.getters import get_product_by_quota
 from bot.database.operations.request.getters import get_started_requests_by_user_id_and_product_id
-from bot.database.operations.request.updaters import update_request
-from bot.database.operations.request.writers import write_request
 from bot.database.operations.user.getters import get_user
 from bot.database.operations.user.updaters import update_user
 from bot.helpers.getters.get_quota_by_model import get_quota_by_model
 from bot.helpers.getters.get_switched_to_ai_model import get_switched_to_ai_model
+from bot.helpers.senders.send_ai_model_internal_error import send_internal_ai_model_error
 from bot.helpers.senders.send_error_info import send_error_info
 from bot.integrations.replicate_ai import create_stable_diffusion_image
 from bot.keyboards.ai.model import build_switched_to_ai_keyboard
 from bot.keyboards.ai.stable_diffusion import build_stable_diffusion_keyboard
 from bot.keyboards.common.common import build_error_keyboard
-from bot.locales.main import get_user_language, get_localization
+from bot.locales.main import get_localization, get_user_language
 from bot.locales.translate_text import translate_text
 from bot.locales.types import LanguageCode
-from replicate.exceptions import ReplicateError
-from bot.helpers.senders.send_ai_model_internal_error import send_internal_ai_model_error
+from bot.utils.ctx_managers.generation_record_ctx import GenerationRecordCtx
+from bot.utils.ctx_managers.processing_msgs_ctx import ProcessingMsgsCtx
+from bot.utils.ctx_managers.request_record_ctx import RequestRecordCtx
 
 stable_diffusion_router = Router()
 
@@ -146,57 +144,61 @@ async def handle_stable_diffusion(
     user_quota: Quota,
     image_filename: Optional[str] = None,
 ):
+    # Params
     user_language_code = await get_user_language(user.id, state.storage)
     user_data = await state.get_data()
 
-    prompt = user_data.get('recognized_text', None)
+    prompt = user_data.get("recognized_text", None)
     if prompt is None:
         if message.caption:
             prompt = message.caption
         elif message.text:
             prompt = message.text
         else:
-            prompt = ''
+            prompt = ""
 
     image_link = None
     if image_filename:
-        image_path = f'users/vision/{user.id}/{image_filename}'
+        image_path = f"users/vision/{user.id}/{image_filename}"
         image = await firebase.bucket.get_blob(image_path)
         image_link = firebase.get_public_url(image.name)
 
-    processing_sticker = await message.answer_sticker(
-        sticker=config.MESSAGE_STICKERS.get(MessageSticker.IMAGE_GENERATION),
-    )
-    processing_message = await message.reply(
-        text=get_localization(user_language_code).model_image_processing_request(),
-        allow_sending_without_reply=True,
-    )
-
-    async with ChatActionSender.upload_photo(bot=message.bot, chat_id=message.chat.id):
-        product = await get_product_by_quota(user_quota)
-
-        user_not_finished_requests = await get_started_requests_by_user_id_and_product_id(user.id, product.id)
-
-        if len(user_not_finished_requests):
-            await message.reply(
-                text=get_localization(user_language_code).MODEL_ALREADY_MAKE_REQUEST,
-                allow_sending_without_reply=True,
-            )
-
-            await processing_sticker.delete()
-            await processing_message.delete()
-            return
-
-        request = await write_request(
-            user_id=user.id,
-            processing_message_ids=[processing_sticker.message_id, processing_message.message_id],
-            product_id=product.id,
-            requested=1,
+    # Validation
+    product = await get_product_by_quota(user_quota)
+    user_not_finished_requests = await get_started_requests_by_user_id_and_product_id(user.id, product.id)
+    if len(user_not_finished_requests):
+        return await message.reply(
+            text=get_localization(user_language_code).MODEL_ALREADY_MAKE_REQUEST,
+            allow_sending_without_reply=True,
         )
 
-        try:
+    # Generation
+    try:
+        async with AsyncExitStack() as stack:
+            # Prepare ctxs
+            await stack.enter_async_context(ChatActionSender.upload_photo(bot=message.bot, chat_id=message.chat.id))
+            processing_msgs_ctx = await stack.enter_async_context(
+                ProcessingMsgsCtx(
+                    message,
+                    config.MESSAGE_STICKERS.get(MessageSticker.IMAGE_GENERATION),
+                    get_localization(user_language_code).model_image_processing_request(),
+                ),
+            )
+            request_record_ctx = await stack.enter_async_context(
+                RequestRecordCtx(
+                    user_id=user.id,
+                    processing_message_ids=processing_msgs_ctx.ids,
+                    product_id=product.id,
+                    requested=1,
+                ),
+            )
+            gen_ctx = await stack.enter_async_context(GenerationRecordCtx())
+
+            # Translate
             if user_language_code != LanguageCode.EN:
                 prompt = await translate_text(prompt, user_language_code, LanguageCode.EN)
+
+            # Send generation
             result_id = await create_stable_diffusion_image(
                 prompt,
                 user.settings[Model.STABLE_DIFFUSION][UserSettings.VERSION],
@@ -204,63 +206,43 @@ async def handle_stable_diffusion(
                 image_link,
             )
 
-            await write_generation(
-                id=result_id,
-                request_id=request.id,
-                product_id=product.id,
-                has_error=result_id is None,
-                details={
-                    'prompt': prompt,
-                }
-            )
-        except ReplicateError as e:
-            if e.status == 500:
-                await send_internal_ai_model_error(
-                    user_language_code, message, Model.STABLE_DIFFUSION
-                )
-        except Exception as e:
-            await message.answer_sticker(
-                sticker=config.MESSAGE_STICKERS.get(MessageSticker.ERROR),
-            )
-
-            await message.answer(
-                text=get_localization(user_language_code).ERROR,
-                reply_markup=build_error_keyboard(user_language_code),
-            )
-            await send_error_info(
-                bot=message.bot,
-                user_id=user.id,
-                info=str(e),
-                hashtags=['stable_diffusion'],
-            )
-
-            request.status = RequestStatus.FINISHED
-            await update_request(request.id, {
-                'status': request.status
-            })
-
-            generations = await get_generations_by_request_id(request.id)
-            for generation in generations:
-                generation.status = GenerationStatus.FINISHED
-                generation.has_error = True
-                await update_generation(
-                    generation.id,
-                    {
-                        'status': generation.status,
-                        'has_error': generation.has_error,
+            gen_ctx.add(
+                await write_generation(
+                    id=result_id,
+                    request_id=request_record_ctx.request.id,
+                    product_id=product.id,
+                    has_error=result_id is None,
+                    details={
+                        "prompt": prompt,
                     },
-                )
-
-            await processing_sticker.delete()
-            await processing_message.delete()
-
-    asyncio.create_task(
-        handle_stable_diffusion_example(
-            user=user,
-            user_language_code=user_language_code,
-            prompt=prompt,
-            message=message,
+                ),
+            )
+    except ReplicateError as e:
+        if e.status == 500:
+            await send_internal_ai_model_error(user_language_code, message, Model.STABLE_DIFFUSION)
+        else:
+            raise
+    except Exception as e:
+        await message.answer_sticker(
+            sticker=config.MESSAGE_STICKERS.get(MessageSticker.ERROR),
         )
+
+        await message.answer(
+            text=get_localization(user_language_code).ERROR,
+            reply_markup=build_error_keyboard(user_language_code),
+        )
+        await send_error_info(
+            bot=message.bot,
+            user_id=user.id,
+            info=str(e),
+            hashtags=["stable_diffusion"],
+        )
+
+    await handle_stable_diffusion_example(
+        user=user,
+        user_language_code=user_language_code,
+        prompt=prompt,
+        message=message,
     )
 
 
@@ -270,68 +252,64 @@ async def handle_stable_diffusion_example(
     prompt: str,
     message: Message,
 ):
+    # Validation
     current_date = datetime.now(timezone.utc)
-    if (
-        not user.subscription_id and
-        user.current_model == Model.STABLE_DIFFUSION and
-        user.settings[user.current_model][UserSettings.VERSION] == StableDiffusionVersion.XL and
-        user.settings[user.current_model][UserSettings.SHOW_EXAMPLES] and
-        user.daily_limits[Quota.STABLE_DIFFUSION_XL] in [1] and
-        (current_date - user.last_subscription_limit_update).days <= 3
+    if not (
+        not user.subscription_id
+        and user.current_model == Model.STABLE_DIFFUSION
+        and user.settings[user.current_model][UserSettings.VERSION] == StableDiffusionVersion.XL
+        and user.settings[user.current_model][UserSettings.SHOW_EXAMPLES]
+        and user.daily_limits[Quota.STABLE_DIFFUSION_XL] in [1]
+        and (current_date - user.last_subscription_limit_update).days <= 3
     ):
-        product = await get_product_by_quota(Quota.STABLE_DIFFUSION_3)
+        return
 
-        request = await write_request(
-            user_id=user.id,
-            processing_message_ids=[message.message_id],
-            product_id=product.id,
-            requested=1,
-            details={
-                'prompt': prompt,
-                'is_suggestion': True,
-            }
-        )
+    # Generate
+    product = await get_product_by_quota(Quota.STABLE_DIFFUSION_3)
+    try:
+        async with AsyncExitStack() as stack:
+            # Prepare ctxs
+            request_record_ctx = await stack.enter_async_context(
+                RequestRecordCtx(
+                    user_id=user.id,
+                    processing_message_ids=[message.message_id],
+                    product_id=product.id,
+                    requested=1,
+                    details={
+                        "prompt": prompt,
+                        "is_suggestion": True,
+                    },
+                ),
+            )
+            gen_ctx = await stack.enter_async_context(GenerationRecordCtx())
 
-        try:
+            # Translate
             if user_language_code != LanguageCode.EN:
                 prompt = await translate_text(prompt, user_language_code, LanguageCode.EN)
+
+            # Send generation
             result_id = await create_stable_diffusion_image(
                 prompt,
                 StableDiffusionVersion.V3,
                 user.settings[Model.STABLE_DIFFUSION][UserSettings.ASPECT_RATIO],
             )
 
-            await write_generation(
-                id=result_id,
-                request_id=request.id,
-                product_id=product.id,
-                has_error=result_id is None,
-                details={
-                    'prompt': prompt,
-                    'is_suggestion': True,
-                }
-            )
-        except Exception as e:
-            await send_error_info(
-                bot=message.bot,
-                user_id=user.id,
-                info=str(e),
-                hashtags=['stable_diffusion', 'example'],
-            )
-
-            request.status = RequestStatus.FINISHED
-            await update_request(request.id, {
-                'status': request.status
-            })
-
-            generations = await get_generations_by_request_id(request.id)
-            for generation in generations:
-                generation.status = GenerationStatus.FINISHED
-                generation.has_error = True
-                await update_generation(
-                    generation.id,
-                    {
-                        'status': generation.status,
-                        'has_error': generation.has_error,
+            gen_ctx.add(
+                await write_generation(
+                    id=result_id,
+                    request_id=request_record_ctx.request.id,
+                    product_id=product.id,
+                    has_error=result_id is None,
+                    details={
+                        "prompt": prompt,
+                        "is_suggestion": True,
                     },
-                )
+                ),
+            )
+    except Exception as e:
+        await send_error_info(
+            bot=message.bot,
+            user_id=user.id,
+            info=str(e),
+            hashtags=["stable_diffusion", "example"],
+        )
