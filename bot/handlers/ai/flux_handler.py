522354109c00fbb6143d037bf1119a1573b974cc
Peter Runich <p.runich@bk.ru>
2025-05-20 18:34:57 +0300

    Flux use ctxs

--- bot/handlers/ai/flux_handler.py
+++ bot/handlers/ai/flux_handler.py
@@ -1,5 +1,7 @@
 import asyncio
+from contextlib import AsyncExitStack
 from datetime import datetime, timezone
+import logging
 from typing import Optional
 
 from aiogram import Router
@@ -37,6 +39,10 @@ from replicate.exceptions import ReplicateError
 from bot.helpers.senders.send_ai_model_internal_error import send_internal_ai_model_error
 from bot.helpers.notifiers.notify_error_channel import notify_error_channel
 import traceback
+
+from bot.utils.ctx_managers.generation_record_ctx import GenerationRecordCtx
+from bot.utils.ctx_managers.processing_msgs_ctx import ProcessingMsgsCtx
+from bot.utils.ctx_managers.request_record_ctx import RequestRecordCtx
 
 
 flux_router = Router()
@@ -140,7 +146,6 @@ async def flux_choose_selection(callback_query: CallbackQuery, state: FSMContext
 
     await state.clear()
 
-
 async def handle_flux(
     message: Message,
     state: FSMContext,
@@ -148,57 +153,61 @@ async def handle_flux(
     user_quota: Quota,
     image_filename: Optional[str] = None,
 ):
+    # Params
     user_language_code = await get_user_language(user.id, state.storage)
     user_data = await state.get_data()
-
-    prompt = user_data.get('recognized_text', None)
+    prompt = user_data.get("recognized_text", None)
     if prompt is None:
         if message.caption:
             prompt = message.caption
         elif message.text:
             prompt = message.text
         else:
-            prompt = ''
+            prompt = ""
 
+    # Validation
+    product = await get_product_by_quota(user_quota)
+    user_not_finished_requests = await get_started_requests_by_user_id_and_product_id(user.id, product.id)
+    if len(user_not_finished_requests):
+        return await message.reply(
+            text=get_localization(user_language_code).MODEL_ALREADY_MAKE_REQUEST,
+            allow_sending_without_reply=True,
+        )
+
+    # Save image for supervision
     image_link = None
     if image_filename:
-        image_path = f'users/vision/{user.id}/{image_filename}'
+        image_path = f"users/vision/{user.id}/{image_filename}"
         image = await firebase.bucket.get_blob(image_path)
         image_link = firebase.get_public_url(image.name)
 
-    processing_sticker = await message.answer_sticker(
-        sticker=config.MESSAGE_STICKERS.get(MessageSticker.IMAGE_GENERATION),
-    )
-    processing_message = await message.reply(
-        text=get_localization(user_language_code).model_image_processing_request(),
-        allow_sending_without_reply=True,
-    )
-
-    async with ChatActionSender.upload_photo(bot=message.bot, chat_id=message.chat.id):
-        product = await get_product_by_quota(user_quota)
-
-        user_not_finished_requests = await get_started_requests_by_user_id_and_product_id(user.id, product.id)
-
-        if len(user_not_finished_requests):
-            await message.reply(
-                text=get_localization(user_language_code).MODEL_ALREADY_MAKE_REQUEST,
-                allow_sending_without_reply=True,
+    # Generation
+    try:
+        async with AsyncExitStack() as stack:
+            # Prepare ctxs
+            await stack.enter_async_context(ChatActionSender.upload_photo(bot=message.bot, chat_id=message.chat.id))
+            processing_msgs_ctx = await stack.enter_async_context(
+                ProcessingMsgsCtx(
+                    message,
+                    config.MESSAGE_STICKERS.get(MessageSticker.IMAGE_GENERATION),
+                    get_localization(user_language_code).model_image_processing_request(),
+                ),
             )
+            request_record_ctx = await stack.enter_async_context(
+                RequestRecordCtx(
+                    user_id=user.id,
+                    processing_message_ids=processing_msgs_ctx.ids,
+                    product_id=product.id,
+                    requested=1,
+                ),
+            )
+            gen_ctx = await stack.enter_async_context(GenerationRecordCtx())
 
-            await processing_sticker.delete()
-            await processing_message.delete()
-            return
-
-        request = await write_request(
-            user_id=user.id,
-            processing_message_ids=[processing_sticker.message_id, processing_message.message_id],
-            product_id=product.id,
-            requested=1,
-        )
-
-        try:
+            # Translate
             if user_language_code != LanguageCode.EN:
                 prompt = await translate_text(prompt, user_language_code, LanguageCode.EN)
+
+            # Send generation
             result_id = await create_flux_image(
                 prompt,
                 user.settings[Model.FLUX][UserSettings.VERSION],
@@ -207,98 +216,87 @@ async def handle_flux(
                 image_link,
             )
 
-            await write_generation(
-                id=result_id,
-                request_id=request.id,
-                product_id=product.id,
-                has_error=result_id is None,
-                details={
-                    'prompt': prompt,
-                }
-            )
-        except ReplicateError as e:
-            if e.status == 500:
-                await send_internal_ai_model_error(
-                    user_language_code, message, Model.FLUX
-                )
-        except Exception as e:
-            await message.answer_sticker(
-                sticker=config.MESSAGE_STICKERS.get(MessageSticker.ERROR),
-            )
-
-            await message.answer(
-                text=get_localization(user_language_code).ERROR,
-                reply_markup=build_error_keyboard(user_language_code),
-            )
-
-            await notify_error_channel(
-                bot=message.bot,
-                user_id=user.id,
-                info=str(e),
-                stack_trace=traceback.format_exc(),
-                context={"prompt": prompt},
-                hashtags=["flux"],
-            )
-
-            request.status = RequestStatus.FINISHED
-            await update_request(request.id, {"status": request.status})
-
-            generations = await get_generations_by_request_id(request.id)
-            for generation in generations:
-                generation.status = GenerationStatus.FINISHED
-                generation.has_error = True
-                await update_generation(
-                    generation.id,
-                    {
-                        "status": generation.status,
-                        "has_error": generation.has_error,
+            gen_ctx.add(
+                await write_generation(
+                    id=result_id,
+                    request_id=request_record_ctx.request.id,
+                    product_id=product.id,
+                    has_error=result_id is None,
+                    details={
+                        "prompt": prompt,
                     },
-                )
+                ),
+            )
+    except ReplicateError as e:
+        if e.status == 500:
+            await send_internal_ai_model_error(user_language_code, message, Model.FLUX)
+        else:
+            raise
+    except Exception as e:
+        logging.exception("")
 
-            await processing_sticker.delete()
-            await processing_message.delete()
-
-    asyncio.create_task(
-        handle_flux_example(
-            user=user,
-            user_language_code=user_language_code,
-            prompt=prompt,
-            message=message,
+        await message.answer_sticker(
+            sticker=config.MESSAGE_STICKERS.get(MessageSticker.ERROR),
         )
+
+        await message.answer(
+            text=get_localization(user_language_code).ERROR,
+            reply_markup=build_error_keyboard(user_language_code),
+        )
+
+        await notify_error_channel(
+            bot=message.bot,
+            user_id=user.id,
+            info=str(e),
+            stack_trace=traceback.format_exc(),
+            hashtags=["flux"],
+        )
+
+    await handle_flux_example(
+        user=user,
+        user_language_code=user_language_code,
+        prompt=prompt,
+        message=message,
     )
 
 
-async def handle_flux_example(
-    user: User,
-    user_language_code: LanguageCode,
-    prompt: str,
-    message: Message,
-):
+async def handle_flux_example(user: User, user_language_code: LanguageCode, prompt: str, message: Message):
+    # Validation
     current_date = datetime.now(timezone.utc)
-    if (
-        not user.subscription_id and
-        user.current_model == Model.FLUX and
-        user.settings[user.current_model][UserSettings.VERSION] == FluxVersion.V1_Dev and
-        user.settings[user.current_model][UserSettings.SHOW_EXAMPLES] and
-        user.daily_limits[Quota.FLUX_1_DEV] in [1] and
-        (current_date - user.last_subscription_limit_update).days <= 3
+    if not (
+        not user.subscription_id
+        and user.current_model == Model.FLUX
+        and user.settings[user.current_model][UserSettings.VERSION] == FluxVersion.V1_Dev
+        and user.settings[user.current_model][UserSettings.SHOW_EXAMPLES]
+        and user.daily_limits[Quota.FLUX_1_DEV] in [1]
+        and (current_date - user.last_subscription_limit_update).days <= 3
     ):
-        product = await get_product_by_quota(Quota.FLUX_1_PRO)
+        return
 
-        request = await write_request(
-            user_id=user.id,
-            processing_message_ids=[message.message_id],
-            product_id=product.id,
-            requested=1,
-            details={
-                'prompt': prompt,
-                'is_suggestion': True,
-            }
-        )
+    # Generation
+    product = await get_product_by_quota(Quota.FLUX_1_PRO)
+    try:
+        async with AsyncExitStack() as stack:
+            # Prepare ctxs
+            request_record_ctx = await stack.enter_async_context(
+                RequestRecordCtx(
+                    user_id=user.id,
+                    processing_message_ids=[message.message_id],
+                    product_id=product.id,
+                    requested=1,
+                    details={
+                        "prompt": prompt,
+                        "is_suggestion": True,
+                    },
+                ),
+            )
+            gen_ctx = await stack.enter_async_context(GenerationRecordCtx())
 
-        try:
+            # Translate
             if user_language_code != LanguageCode.EN:
                 prompt = await translate_text(prompt, user_language_code, LanguageCode.EN)
+
+            # Send generation
             result_id = await create_flux_image(
                 prompt,
                 FluxVersion.V1_Pro,
@@ -306,41 +304,27 @@ async def handle_flux_example(
                 user.settings[Model.FLUX][UserSettings.SAFETY_TOLERANCE],
             )
 
-            await write_generation(
-                id=result_id,
-                request_id=request.id,
-                product_id=product.id,
-                has_error=result_id is None,
-                details={
-                    'prompt': prompt,
-                    'is_suggestion': True,
-                }
-            )
-        except Exception as e:
-            await notify_error_channel(
-                bot=message.bot,
-                user_id=user.id,
-                info=str(e),
-                stack_trace=traceback.format_exc(),
-                context={"prompt": prompt},
-                hashtags=["flux", "example"],
-            )
-
-            request.status = RequestStatus.FINISHED
-            await update_request(request.id, {
-                'status': request.status
-            })
-
-            generations = await get_generations_by_request_id(request.id)
-            for generation in generations:
-                generation.status = GenerationStatus.FINISHED
-                generation.has_error = True
-                await update_generation(
-                    generation.id,
-                    {
-                        'status': generation.status,
-                        'has_error': generation.has_error,
+            # Send generation
+            gen_ctx.add(
+                await write_generation(
+                    id=result_id,
+                    request_id=request_record_ctx.request.id,
+                    product_id=product.id,
+                    has_error=result_id is None,
+                    details={
+                        "prompt": prompt,
+                        "is_suggestion": True,
                     },
-                )
+                ),
+            )
+    except Exception as e:
+        logging.exception("")
 
+        await notify_error_channel(
+            bot=message.bot,
+            user_id=user.id,
+            info=str(e),
+            stack_trace=traceback.format_exc(),
+            hashtags=["flux", "example"],
+        )
 
